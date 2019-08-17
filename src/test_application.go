04fadd52f8432912b758fe6cfbc109ae57b05539
Antônio Gabriel <antoniogabrielleal@gmail.com>
2019-08-17 01:53:41 -0300

    Initial Commit

---
+++ src/test_application.go
@@ -0,0 +1,13 @@
+package src
+
+import "fmt"
+
+func main() {
+
+	/*Pritando "Hello World" no console*/
+	fmt.Println("Hello World")
+
+	/*Pritando a conta no console*/
+	var test = 43 + 43
+	fmt.Println(test)
+}
